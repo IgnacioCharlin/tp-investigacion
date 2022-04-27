e0f7eb5a1baf5b91f59fd938f26bc728afb6c029
ignaciocharlin1995@gmail.com <ignaciocharlin1995@gmail.com>
2022-04-27 14:47:04 -0300

    primer commit

---
+++ Prueba/Clases/CConexion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace Prueba.Clases
+{
+    class CConexion
+    {
+        static string servidor= "localhost";
+        static string puerto= "27017";
+
+        public static MongoClient cliente = new MongoClient($"mongodb://{servidor}:{puerto}");
+
+        public MongoClient establecerConexion()
+        {
+            try
+            {
+                List<String> NombresBasesDeDatos = cliente.ListDatabaseNames().ToList();
+                foreach(var db in NombresBasesDeDatos)
+                {
+                    Console.WriteLine($"Se conecto correctamente a : {db}");
+                }
+            }
+            catch (MongoClientException e){
+                Console.WriteLine("No se logro conectar a mongoDb , error"+e.ToString());
+            }
+
+            return cliente;
+        }
+    }
+}
---
+++ Prueba/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    internal class Usuario
+    {
+        public long Id { get; set; }
+
+        public string Nombre { get; set; }    
+
+        public string Apellido { get; set; }
+
+        public string Sexo { get; set; }
+
+
+    }
+}
+
